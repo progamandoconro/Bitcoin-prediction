e89a15e174ed840c156054dd5a074892d77fe75b
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-22 23:21:27 +0100

    Delete update_data.sh

--- update_data.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/progamandoconro/Bitcoin-prediction
-
-lynx -dump  https://coinmetrics.io/data-downloads/ | \
-awk '/csv/{print $2}' > urls.txt  \
-&& wget -N urls.txt -i 
