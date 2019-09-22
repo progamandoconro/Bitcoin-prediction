ad805afc21f9969bc32beabe6e25f477d853587d
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-09-22 20:08:26 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+ 
+<header>
+
+<center>
+
+<?php
+$product = "Coin-Predictor";
+
+ echo "<h1>$product<h1>";
+
+?>
+<img src="https://programandoconro.files.wordpress.com/2019/07/cropped-net-2.png?w=300">
+<h5>Free sofware for future's price predictions of 79 cryptocoins<h5> 
+<header>
+
+<body>
+<div align="left">
+  List of available coins:
+</div>
+
+
+<body>
+
+ <html>
