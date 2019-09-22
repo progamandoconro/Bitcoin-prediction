233243e39775d248691456645d364cf52a249daa
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-09-22 23:57:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <html>
-
- 
 <header>
-
 <center>
-
 <?php
 $product = "Coin-Predictor";
 
@@ -18,9 +14,89 @@ $product = "Coin-Predictor";
 <body>
 <div align="left">
   List of available coins:
+
+<ul>
+<li>    0x
+   <li> Aelf
+    <li>Aeternity (pre-mainnet to 2018-11-13)
+    <li>Aion (pre-mainnet to 2018-09-18)
+    <li>Aragon (Partial Support)
+    <li>Augur
+    <li>Basic Attention Token
+    <li>Binance Coin (pre-mainnet to 2019-04-22)
+    <li>Binance Coin
+    <li>Bitcoin Cash
+    <li>Bitcoin Gold
+    <li>Bitcoin SV
+    <li>Bitcoin
+    <li>Bytom (pre-mainnet to 2018-04-23)
+    <li>Cardano
+   <li> Chainlink
+    <li>Civic
+    <li>Dai
+    <li>Dash
+    <li>Decentraland
+    <li>Decred (Partial Support)
+    <li>Digibyte
+    <li>Dogecoin
+    <li>Dragonchain (Partial Support)
+    <li>Enigma
+    <li>EOS (Partial Support)
+    <li>EOS (ERC20 era)
+    <li>Ethereum Classic
+    <li>Ethereum
+    <li>Ethos
+    <li>FunFair
+    <li>Gas
+    <li>Gemini Dollar
+    <li>Gnosis
+    <li>Golem
+    <li>Grin
+    <li>ICON (pre-mainnet to 2018-06-24)
+    <li>KuCoin Shares (Partial Support)
+    <li>Kyber Network
+    <li>Lisk
+    <li>Litecoin
+    <li>Loom Network
+    <li>Loopring
+    <li>MaidSafeCoin
+    <li>Maker
+    <li> Monero
+    <li>Nebulas (pre-mainnet to 2018-05-01)
+    <li>NEM
+    <li>NEO
+    <li>OmiseGO
+    <li>Paxos Standard Token
+    <li>PIVX
+    <li>Polymath
+    <li>Populous
+    <li>Power Ledger
+    <li>QASH
+    <li>QTUM
+    <li>Status
+    <li>Stellar
+    <li>TenX
+    <li>Tether (Ethereum)
+    <li>Tether (Omni)
+    <li>Tezos
+    <li>TRON (Partial Support)
+    <li>TrueUSD
+    <li>UNUS SED LEO (ETH)
+    <li>UNUS SED LEO (EOS)
+    <li>USD Coin
+    <li>VeChain (pre-mainnet to 2018-06-30)
+    <li>Verge
+    <li>Vertcoin (Partial Support)
+   <li>Waltonchain
+    <li>WAVES
+    <li>XRP
+    <li>ZCash
+    <li>Zilliqa (pre-mainnet to 2019-04-21)
+
+</ul>
 </div>
-
-
 <body>
-
  <html>
+
+
+
